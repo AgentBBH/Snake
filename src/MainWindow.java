169510c3433044389ccec3def514ff88d03a69f1
Marina <marina2013@tut.by>
2019-11-15 21:28:31 +0300

    first commit

---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("Snake");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(640, 640);
+        setLocation(350, 60);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
